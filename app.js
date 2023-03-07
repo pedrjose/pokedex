50ef2a27489b2dc640357769c51f443cb7f85172
pedro josé lima <pedrlimadev@gmail.com>
2023-03-07 12:40:47 -0300

    PokeDex, Base Structure - Finished

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
+const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`;
+
 const fetchPokemon = () => {
-    const url = `https://pokeapi.co/api/v2/pokemon`;
-    fetch(url);
+
+    const pokemonPromises = [];
+
+    for (i = 1; i <= 150; i++) {
+        pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()));
+    }
+
+    Promise.all(pokemonPromises)
+        .then(pokemons => {
+            const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
+                const types = pokemon.types.map(typeInfo => typeInfo.type.name);
+
+                // correct logic bug
+                accumulator += `
+                <li class="card ${types[0]}">
+                <img class="card-image" alt="${pokemon.name}" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/00${pokemon.id}.png"/>
+                    <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+                    <p card="card-subtitle">${types.join(' | ')}</p>
+                </li>
+                `
+                return accumulator;
+            }, '')
+
+            const ul = document.querySelector('[data-js="pokedex"')
+
+            ul.innerHTML = lisPokemons;
+        })
 }
 
 fetchPokemon();
