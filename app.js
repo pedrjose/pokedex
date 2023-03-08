74ca5f4d5fc29735fea3ac836642adf41c8a38e4
pedro josé lima <pedrlimadev@gmail.com>
2023-03-08 11:50:20 -0300

    Comments

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
+/* getPokemonUrl' is the function responsible to get the pokemons data through the API */ 
 const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`;
 
+/*
+'generatePokemonPromises' is responsible to create a array with 150 pokemons promises
+pokemons data are get through the function 'getPokemonUrl'
+*/
 const generatePokemonPromises = () => {
     const pokemonPromises = [];
 
+    // 'fetch' do a asynchronous request to API encapsulated in 'getPokemonUrl' with array index
     for (i = 1; i <= 150; i++) {
         pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()));
     }
 
+    // the array will return when filed with 150 pokemons
     return pokemonPromises;
 }
 
+/* Insert the pokemon card in HTML page */
 const insertPokemonCardIntoPage = pokemons => {
     const ul = document.querySelector('[data-js="pokedex"')
     ul.innerHTML = pokemons;
 }
 
+/* Generate li tag with pokemon card */
 const generateHTML = pokemons => {
     return pokemons.reduce((accumulator, pokemon) => {
         const types = pokemon.types.map(typeInfo => typeInfo.type.name);
