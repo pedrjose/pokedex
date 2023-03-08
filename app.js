109664a491d38d87e64cbed2ce63ad1083b6b617
pedro josé lima <pedrlimadev@gmail.com>
2023-03-08 11:15:07 -0300

    PokeDex Project Finished + Code Refactoring

--- app.js
+++ app.js
@@ -1,33 +1,38 @@
 const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`;
 
-const fetchPokemon = () => {
-
+const generatePokemonPromises = () => {
     const pokemonPromises = [];
 
     for (i = 1; i <= 150; i++) {
         pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()));
     }
 
-    Promise.all(pokemonPromises)
-        .then(pokemons => {
-            const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
-                const types = pokemon.types.map(typeInfo => typeInfo.type.name);
-
-                // correct logic bug
-                accumulator += `
-                <li class="card ${types[0]}">
-                <img class="card-image" alt="${pokemon.name}" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/00${pokemon.id}.png"/>
-                    <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
-                    <p card="card-subtitle">${types.join(' | ')}</p>
-                </li>
-                `
-                return accumulator;
-            }, '')
-
-            const ul = document.querySelector('[data-js="pokedex"')
-
-            ul.innerHTML = lisPokemons;
-        })
+    return pokemonPromises;
 }
 
-fetchPokemon();
+const insertPokemonCardIntoPage = pokemons => {
+    const ul = document.querySelector('[data-js="pokedex"')
+    ul.innerHTML = pokemons;
+}
+
+const generateHTML = pokemons => {
+    return pokemons.reduce((accumulator, pokemon) => {
+        const types = pokemon.types.map(typeInfo => typeInfo.type.name);
+
+        accumulator += `
+        <li class="card ${types[0]}">
+        <img class="card-image" alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"/>
+            <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+            <p card="card-subtitle">${types.join(' | ')}</p>
+        </li>
+        `
+
+        return accumulator;
+    }, '')
+}
+
+pokemonPromises = generatePokemonPromises();
+
+Promise.all(pokemonPromises)
+    .then(generateHTML)
+    .then(insertPokemonCardIntoPage)
